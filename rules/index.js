d46eb16e7048f557e3ab5f621b249b858c54fb22
fakership <lidong19@meituan.com>
2017-08-10 10:12:38 +0800

    update

--- rules/index.js
+++ rules/index.js
@@ -1,6 +1,5 @@
 module.exports = {
 	rules: {
-		'no-restricted-imports': [2, 'import1', 'import2'], // import引入
 		'import/no-extraneous-dependencies': 0,
 		'import/extensions': 0,
 		'import/no-unresolved': 0,
@@ -14,21 +13,18 @@ module.exports = {
 		'no-restricted-syntax': [2, 'BinaryExpression[operator="in"]'], // 是否能用in
 		'quote-props': [2, 'consistent'], // 对象属性名可以不使用引号
 		'no-param-reassign': 0, // 允许参数改变
-		'comma-dangle': [2, 'never'], // 对象最后一个属性值不需要逗号
 		'object-shorthand': 0, // 对象和值名可以相同
-		'no-console': 0, // console
+		'no-console': process.env.NODE_ENV === 'production' ? 2 : 0, // console
 		'no-bitwise': [2, { 'allow': ['~'] }], // 允许使用~
 		'no-underscore-dangle': 0, // 允许使用_
 		'radix': 0, // 允许parseInt不写进制数
 		'max-len': [2, { 'code': 150 }], // 最大长度
 		'no-lonely-if': 0, // 不仅仅是一个if
 		'dot-notation': 0, // 不要求使用点号
-		'no-undef': 0, // 允许未定义的KNB
 		'no-mixed-operators': 0, // 允许多个操作符
 		'no-plusplus': 0, // 允许++
 		'no-unused-expressions': 0, // 允许未使用过的表达式
 		'no-unneeded-ternary': 0, // 允许三元运算符
-		'no-shadow': 0, // 参数重复定义
 		'prefer-arrow-callback': 0,
 		'arrow-body-style': 0,
 		'guard-for-in': 0, // 允许for in不做处理
